8f891376643b564a779cc410dc93655d8ebb0d42
Naser.Hosseini <naser.hosseini@gmail.com>
2022-01-01 13:15:39 -0600

    update the puthon file

--- DataCollection.py
+++ DataCollection.py
@@ -11,8 +11,6 @@ def load_data(table,data):
     for x in mycrs.fetchall():
         col+=x[0]+','
     col=col[:-1]
-    print(col)
-    print(data)
     sql='INSERT INTO {}({}) VALUES{};'.format(table,col,data)
     print(sql)
     mycrs.execute(sql)
@@ -48,7 +46,7 @@ except mysql.connector.Error as err:
 Initialize dimension table
 '''
 
-#Product
+#Products
 sql='SELECT distinct product_id, product_name FROM minioop.products;'
 mycrs.execute(sql)
 rows=mycrs.fetchall()
@@ -56,14 +54,21 @@ product={}
 for row in rows:
     product[row[1]]=row[0]
 
-
-#Type
-sql='SELECT type_id, type FROM minioop.products;'
+#Types
+sql='SELECT type_id, type FROM products;'
 mycrs.execute(sql)
 rows=mycrs.fetchall()
 type_name={}
 for row in rows:
     type_name[row[1]]=row[0]
+
+#Materials
+sql='SELECT * FROM material;'
+mycrs.execute(sql)
+rows=mycrs.fetchall()
+mat={}
+for row in rows:
+    mat[row[1].lower()]=row[0]
 
 path='D:/DE/Materials/Projects/Chapter02/'
 parent=os.listdir(path)
@@ -74,12 +79,75 @@ for x in parent:
             rows=csv.reader(sells,delimiter=',')
             next(rows)
             for row in rows:
-                #row.insert(0,row[4])
                 row=[row[4],row[0],product[row[1]],type_name[row[2]],row[3]]
                 delivery=datetime.strptime(row[1],'%Y-%m-%d')+timedelta(days=7)
                 delivery=delivery.strftime('%Y-%m-%d')
                 row.append(delivery)
                 row.append(1)
                 data=tuple(row)
-                print(data)
-#                load_data('orders',data=data)
+                load_data('orders',data=data)
+
+    elif x.startswith('Purchase'):
+        table='inventory'
+        f=path +'/'+x
+        with open(f) as sells:
+            rows=csv.reader(sells,delimiter=',')
+            next(rows)
+            for row in rows:
+                if row[1]:
+                    data=[row[0] ,mat[row[8]] , row[1]]
+                    load_data(table,tuple(data))
+                if row[2]:
+                    data=[row[0] ,mat['milk'] , row[2]]
+                    load_data(table,tuple(data))
+                if row[3]:
+                    data=[row[0] ,mat['egg'] , row[2]]
+                    load_data(table,tuple(data))
+                if row[4]:
+                    data=[row[0] ,mat['oil'] , row[4]]
+                    load_data(table,tuple(data))
+                if row[5]:
+                    data=[row[0] ,mat['fondant'] , row[5]]
+                    load_data(table,tuple(data))
+                if row[6]:
+                    data=[row[0] ,mat['sugar'] , row[6]]
+                    load_data(table,tuple(data))
+                if row[7]:
+                    data=[row[0] ,mat['butter'] , row[7]]
+                    load_data(table,tuple(data))
+                if row[9]:
+                    data=[row[0] ,mat['flour'] , row[9]]
+                    load_data(table,tuple(data))
+
+    elif x.startswith('Consume'):
+        table = 'consume'
+        f = path + '/' + x
+        with open(f) as sells:
+            rows = csv.reader(sells, delimiter=',')
+            next(rows)
+            for row in rows:
+                if row[1]:
+                    data = [row[0], mat[row[8]], row[1]]
+                    load_data(table, tuple(data))
+                if row[2]:
+                    data = [row[0], mat['milk'], row[2]]
+                    load_data(table, tuple(data))
+                if row[3]:
+                    data = [row[0], mat['egg'], row[2]]
+                    load_data(table, tuple(data))
+                if row[4]:
+                    data = [row[0], mat['oil'], row[4]]
+                    load_data(table, tuple(data))
+                if row[5]:
+                    data = [row[0], mat['fondant'], row[5]]
+                    load_data(table, tuple(data))
+                if row[6]:
+                    data = [row[0], mat['sugar'], row[6]]
+                    load_data(table, tuple(data))
+                if row[7]:
+                    data = [row[0], mat['butter'], row[7]]
+                    load_data(table, tuple(data))
+                if row[9]:
+                    data = [row[0], mat['flour'], row[9]]
+                    load_data(table, tuple(data))
+
